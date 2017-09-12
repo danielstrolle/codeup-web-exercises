dd95b50cebb30c22ec9fb07ab4c553243105e42c
danielstrolle <danielstrolle@gmail.com>
2017-09-12 15:56:04 -0500

    add other ways to do some functionality

--- js/promises.js
+++ js/promises.js
@@ -12,22 +12,44 @@
         });
     }
 
+    //ANOTHER WAY TO ACCOMPLISH THE SAME FUNCTIONALITY //
+    // const wait = (milliseconds) => {
+    //     return new Promise((resolve, reject) => {
+    //         setTimeout(() => {
+    //             resolve();
+    //         }, milliseconds);
+    //     });
+    // };
+
     wait(1000).then(() => console.log('You\'ll see this after 1 second'));
     wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
     function lastCommit(username) {
         return fetch(
             `https://api.github.com/users/${username}/events`,
-            {headers: {'Authorization': 'token 9a0b5cc70f28ec51b12ead281ee35e7141805cd3'}}
+            {headers: {'Authorization': 'token MY_TOKEN'}}
         )
             .then((response) => response.json())
             .then(events => console.log(events[0].created_at));
     }
-
     lastCommit("danielstrolle");
 
 
-// .then((response) => console.log(response.json())
-//         .then(users => users.map(user => user.created_at));
+    // ANOTHER WAY TO ACCOMPLISH THE SAME FUNCTIONALITY //
+    // const optionalHeaders = {
+    //     headers: {
+    //         "Authorization": "token MY_TOKEN"
+    //     }
+    // };
 
+    // const getCommit = (username) => {
+    //     return fetch(`https://api.github.com/users/${username}/events`, optionalHeaders)
+    // };
+    // let githubPromise = getCommit("danielstrolle");
+    //
+    // githubPromise.then((data) => {
+    //     data.json().then((response) => {
+    //         console.log(response[0].created_at);
+    //     });
+    // });
 })();
